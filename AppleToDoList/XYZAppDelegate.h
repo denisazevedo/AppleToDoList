9a3d88b2148eaba94c92d9af9229917282e4582e
Denis C de Azevedo <denis.azevedo@gmail.com>
2014-02-10 16:52:22 -0200

    Initial Commit

---
+++ AppleToDoList/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  AppleToDoList
+//
+//  Created by Denis C de Azevedo on 10/02/14.
+//  Copyright (c) 2014 Denis C de Azevedo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AppleToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AppleToDoList
+//
+//  Created by Denis C de Azevedo on 10/02/14.
+//  Copyright (c) 2014 Denis C de Azevedo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
